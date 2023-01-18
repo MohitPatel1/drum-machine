b9572b0ce0b60716af568706bbcb55381e36581d
Mohit <mohit.patel1966@gmail.com>
2023-01-18 16:26:52 +0530

    on/off text change, textBox added

--- src/App.jsx
+++ src/App.jsx
@@ -2,55 +2,73 @@ import { useState } from 'react'
 
 function App() {
   const [power, setPower] = useState("Off")
+  const [textBox, setTextBox] = useState("Welcome")
+
   const audio = new Audio()
 
   const togglePower = ()=> {
-    (power == "Off") ? setPower("On") : setPower("Off") 
+    if(power == "Off") {
+      setPower("On") 
+      setTextBox("Play ")
+    }
+    else{
+      setPower("Off") 
+      setTextBox("Turn on Power")
+    }
   }
-  
   const playMusic = (event) => {
     console.log(event.target.innerHTML)
+    console.log(textBox)
     if(power == "On"){
       switch(event.target.innerHTML){
         case "Q":
           audio.src = "src/music/Heater-1.mp3";
+          setTextBox("Heater 1");
           break;
       
         case "W":
           audio.src = "src/music/Heater-2.mp3";
+          setTextBox("Heater 2");
           break;
 
         case "E":
           audio.src = "src/music/Heater-3.mp3";
+          setTextBox("Heater 3");
           break;
       
         case "A":
           audio.src = "src/music/Heater-4.mp3";
+          setTextBox("Heater 4");
           break;
         
         case "S":
           audio.src = "src/music/Clap.mp3";
+          setTextBox("Clap");
           break;
       
         case "D":
           audio.src = "src/music/Open-HH.mp3";
+          setTextBox("Open HH");
           break;
       
         case "Z":
           audio.src = "src/music/Kick_n_Hat.mp3";
+          setTextBox("Kick n' Hat");
           break;
       
         case "X":
           audio.src = "src/music/Kick.mp3";
+          setTextBox("Kick");
           break;
         
         case "C":
           audio.src = "src/music/Closed-HH.mp3";  
+          setTextBox("Closed HH");
         }
         audio.play();
       }
       else{
-        // turn on drum messege
+        setTextBox("Turn on Power")
       }
     }
 
@@ -70,17 +88,21 @@ function App() {
                 <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>X</p>      
                 <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>C</p>      
               </div>
-              <div className='flex flex-col justify-center p-4 border-l-4 border-white space-y-10'>
-                <div className='flex justify-center gap-2' onClick={togglePower}>
+
+              <div className='flex flex-col justify-center align-middle p-4 border-l-4 border-white space-y-10'>
+
+                <div className='flex justify-center align-middle p-2 gap-2' onClick={togglePower}>
                   <h2>Power</h2>
-                  <button id='power' className='bg-gray-400 rounded-md pl-2 pr-2'>{power}</button>
+                  <button id='power' className='bg-gray-400 rounded-md pl-2 pr-2 text-center'>{power}</button>
                 </div>
-                <div className='flex justify-center p'>
-                  <button className='bg-gray-400 rounded-md p-2'>Audio Name</button>
-                </div>
-                <div>
+
+                <button className='bg-gray-400 rounded-md p-2 h-[50px]'>{textBox}</button>
+
+                <div className='flex flex-col justify-center align-middle p-2 pt-0 gap-2'>
+                  <p className='text-center'>Volume</p>
                   <input id='volume' type="range" min="0" max="100"/>
                 </div>
+
               </div>
             </div>
           </div>
