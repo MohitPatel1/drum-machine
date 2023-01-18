d96aa8c2dbbceb2e92ebf216be598882dd692953
Mohit <mohit.patel1966@gmail.com>
2023-01-18 15:35:47 +0530

    onClick music plays, power on/off working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
 import { useState } from 'react'
 
 function App() {
-  const [play, setPlay] = useState("Q")
+  const [power, setPower] = useState("Off")
+  const audio = new Audio()
+
+  const togglePower = ()=> {
+    (power == "Off") ? setPower("On") : setPower("Off") 
+  }
   
   const playMusic = (event) => {
-    play = event.target.value
-  }
+    console.log(event.target.innerHTML)
+    if(power == "On"){
+      switch(event.target.innerHTML){
+        case "Q":
+          audio.src = "src/music/Heater-1.mp3";
+          break;
+      
+        case "W":
+          audio.src = "src/music/Heater-2.mp3";
+          break;
+
+        case "E":
+          audio.src = "src/music/Heater-3.mp3";
+          break;
+      
+        case "A":
+          audio.src = "src/music/Heater-4.mp3";
+          break;
+        
+        case "S":
+          audio.src = "src/music/Clap.mp3";
+          break;
+      
+        case "D":
+          audio.src = "src/music/Open-HH.mp3";
+          break;
+      
+        case "Z":
+          audio.src = "src/music/Kick_n_Hat.mp3";
+          break;
+      
+        case "X":
+          audio.src = "src/music/Kick.mp3";
+          break;
+        
+        case "C":
+          audio.src = "src/music/Closed-HH.mp3";  
+        }
+        audio.play();
+      }
+      else{
+        // turn on drum messege
+      }
+    }
 
   return (
     <div className="h-screen bg-gray">
@@ -24,9 +71,9 @@ function App() {
                 <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>C</p>      
               </div>
               <div className='flex flex-col justify-center p-4 border-l-4 border-white space-y-10'>
-                <div className='flex justify-center gap-2'>
+                <div className='flex justify-center gap-2' onClick={togglePower}>
                   <h2>Power</h2>
-                  <button id='power' className='bg-gray-400 rounded-md pl-2 pr-2'>On</button>
+                  <button id='power' className='bg-gray-400 rounded-md pl-2 pr-2'>{power}</button>
                 </div>
                 <div className='flex justify-center p'>
                   <button className='bg-gray-400 rounded-md p-2'>Audio Name</button>
