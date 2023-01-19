d5f9f7f279bb161499d14e88271d3c63b5743ae5
Mohit <mohit.patel1966@gmail.com>
2023-01-19 11:47:42 +0530

    common function for click and keyPress

--- src/App.jsx
+++ src/App.jsx
@@ -11,176 +11,128 @@ import Kick from "./music/Kick.mp3"
 import Closed_HH from "./music/Closed_HH.mp3"
 
 function App() {
-// declaring states and audio
+  // declaring states and audio
   const [power, setPower] = useState("Off")
   const [textBox, setTextBox] = useState("Welcome")
   // const [volume, setVolume] = useState("50")
-console.log(clap)
-  
-// toggle power function 
-  const togglePower = ()=> {
-    if(power == "Off") {
-      setPower("On") 
+
+  // toggle power function 
+  const togglePower = () => {
+    if (power == "Off") {
+      setPower("On")
       setTextBox("Play ")
     }
-    else{
-      setPower("Off") 
+    else {
+      setPower("Off")
       setTextBox("Turn on Power")
     }
   }
-// play music function using click
-  const playMusic = (event) => {
-    if(power == "On"){
-      switch(event.target.innerHTML){
+  // play music function
+
+  const playMusic = (input) => {
+    if (power == "On") {
+      switch (input) {
         case ("Q"):
           audio.src = Heater1;
           setTextBox("Heater 1");
           break;
-      
         case "W":
           audio.src = Heater2;
           setTextBox("Heater 2");
           break;
-
         case "E":
           audio.src = Heater3;
           setTextBox("Heater 3");
           break;
-      
         case "A":
           audio.src = Heater4;
           setTextBox("Heater 4");
           break;
-        
         case "S":
           audio.src = clap;
           setTextBox("Clap");
           break;
-      
         case "D":
           audio.src = Open_HH;
           setTextBox("Open HH");
           break;
-      
         case "Z":
           audio.src = Kick_n_Hat;
           setTextBox("Kick n' Hat");
           break;
-      
         case "X":
           audio.src = Kick;
           setTextBox("Kick");
           break;
-        
         case "C":
-          audio.src = Closed_HH;  
+          audio.src = Closed_HH;
           setTextBox("Closed HH");
           break;
-        }
-        audio.play();
+      }
+      audio.play();
+    }
+    else {
+      setTextBox("Turn on Power")
+    }
+  }
+
+  // play music function using click
+  const onClickMusic = (event) => {
+    const click = event.target.innerHTML
+    playMusic(click);
+  }
+
+  // play music function using key press event listener
+  const keyPressMusic = (event) => {
+    const key = event.key.toUpperCase()
+    playMusic(key)
+  }
+
+    // volume change function
+    // const onVolumeChange = () => {
+    //   const volumeElement = document.getElementById("volume");
+    //   volumeElement.addEventListener("change", () => {
+    //     audio.volume = volumeElement.value / 100;
+    //     console.log(audio.volume)
+    //   })
+    // } 
+
+    const volumeElement = document.getElementById("volume");
+    const audio = new Audio()
+
+    const onVolumeChange = () => {
+      audio.volume = volumeElement.value / 100;
+    }
+
+    const onKeyPress = () => {
+      if(power == "On"){
+        window.addEventListener('keydown', keyPressMusic)
+        return () => {window.removeEventListener('keydown', keyPressMusic); }
       }
       else{
         setTextBox("Turn on Power")
       }
     }
 
-// play music function using key press event listener
-const keyPressMusic = (event) => {
-  const key = event.key.toUpperCase()
-  switch(key){
-    case ("Q"):
-      audio.src = Heater1;
-      setTextBox("Heater 1");
-      break;
-  
-    case "W":
-      audio.src = Heater2;
-      setTextBox("Heater 2");
-      break;
+    // use effect hook
+    // useEffect(onVolumeChange, []);
+    useEffect(onKeyPress, [power]);
 
-    case "E":
-      audio.src = Heater3;
-      setTextBox("Heater 3");
-      break;
-  
-    case "A":
-      audio.src = Heater4;
-      setTextBox("Heater 4");
-      break;
-    
-    case "S":
-      audio.src = clap;
-      setTextBox("Clap");
-      break;
-  
-    case "D":
-      audio.src = Open_HH;
-      setTextBox("Open HH");
-      break;
-  
-    case "Z":
-      audio.src = Kick_n_Hat;
-      setTextBox("Kick n' Hat");
-      break;
-  
-    case "X":
-      audio.src = Kick;
-      setTextBox("Kick");
-      break;
-    
-    case "C":
-      audio.src = Closed_HH;  
-      setTextBox("Closed HH");
-      break;
-    }
-    audio.play();
-  }
-  
-  const onKeyPress = () => {
-    if(power == "On"){
-      window.addEventListener('keydown', keyPressMusic)
-      return () => {window.removeEventListener('keydown', keyPressMusic); }
-    }
-    else{
-      setTextBox("Turn on Power")
-    }
-  }
-  
-  // volume change function
-  // const onVolumeChange = () => {
-  //   const volumeElement = document.getElementById("volume");
-  //   volumeElement.addEventListener("change", () => {
-  //     audio.volume = volumeElement.value / 100;
-  //     console.log(audio.volume)
-  //   })
-  // } 
-
-  const volumeElement = document.getElementById("volume");
-  const audio = new Audio()
-
-  const onVolumeChange = () => {
-    audio.volume = volumeElement.value / 100 ;
-  }
-
-// use effect hook
-  // useEffect(onVolumeChange, []);
-  useEffect(onKeyPress,[power]);
-
-  return (
-    <div className="h-screen bg-gray">
-      <div id='drum-machine' className='flex h-full w-full justify-center gap-6 items-center'>
+    return (
+      <div className="h-screen bg-gray">
+        <div id='drum-machine' className='flex h-full w-full justify-center gap-6 items-center'>
           <div className='flex items-center gap-6 bg-gray-300'  >
             <div className='flex gap-2 border-4 border-yellow-400'>
               <div className='bg-gray grid grid-cols-3 p-4 gap-y-4 gap-x-6'>
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>Q</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>W</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>E</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>A</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>S</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>D</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>Z</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>X</p>      
-                <p onClick={playMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>C</p>      
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>Q</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>W</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>E</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>A</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>S</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>D</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>Z</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>X</p>
+                <p onClick={onClickMusic} className='bg-red-500 px-6 py-4 text-center rounded-md drum-pad'>C</p>
               </div>
 
               <div className='flex flex-col justify-center align-middle p-4 border-l-4 border-white space-y-10'>
@@ -194,16 +146,15 @@ const keyPressMusic = (event) => {
 
                 <div className='flex flex-col justify-center align-middle p-2 pt-0 gap-2'>
                   <p className='text-center'>Volume</p>
-                  <input id='volume' type="range" name='volume' onChange={onVolumeChange} min="0" max="100"/>
+                  <input id='volume' type="range" name='volume' onChange={onVolumeChange} min="0" max="100" />
                 </div>
 
               </div>
             </div>
           </div>
+        </div>
       </div>
-    </div>
-  )
+    )
 }
 
-export default App
-
+export default App 
